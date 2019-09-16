62ca825b3435055ccf29326f9959f031019bafc2
Bartek Krupa <krupa1.bk@gmail.com>
2019-09-16 17:33:18 +0200

    add about section

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,65 @@
 
 const nav = document.querySelector(".nav");
 const hamburger = document.querySelector(".hamburger");
+const menuBtns = document.querySelectorAll(".navBtn");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
+});
+
+const hideNav = () => {
+  if (nav.classList.value.includes("active")) {
+    nav.classList.remove("active");
+  }
+};
+
+document.querySelector(".main").addEventListener("click", hideNav);
+
+menuBtns.forEach(btn => {
+  btn.addEventListener("click", hideNav);
+});
+
+//smooth scroll
+
+$(".navBtn").on("click", function(e) {
+  const goToSection = `.${e.target.dataset.target}`;
+  $("body, html").animate({
+    scrollTop: $(goToSection).offset().top
+  });
+});
+
+const closeMenu = () => {
+  if (nav.classList.contains("active")) {
+    nav.classList.remove("active");
+  }
+};
+
+//onscroll animations
+
+const about = document.querySelector(".about");
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY >
+    about.offsetTop + about.offsetHeight - window.innerHeight + 150
+  ) {
+    about.classList.add("active");
+  }
+  // if (
+  //   window.scrollY >
+  //   offer.offsetTop + offer.offsetHeight - window.innerHeight - 100
+  // ) {
+  //   offer.classList.add("active");
+  // }
+  // if (
+  //   window.scrollY >
+  //   contact.offsetTop + contact.offsetHeight - window.innerHeight - 400
+  // ) {
+  //   contact.classList.add("active");
+  // }
+  if (window.scrollY < 100) {
+    about.classList.remove("active");
+    // offer.classList.remove("active");
+    // contact.classList.remove("active");
+  }
 });
